19a33f84568b98fc9eadd6f3f278a5ac210b03eb
Vladyslava Abramenko <vladyslavaabramenko@gmai.com>
2022-08-23 20:39:54 +0300

    Assertions

--- src/test/java/TestS.java
+++ src/test/java/TestS.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,34 +10,46 @@ import java.util.concurrent.TimeUnit;
 public class TestS {
 
     @Test
-    public void check(){
+    public void check() {
 
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
-
-        //  By code = By.xpath("//*[contains(text(), 'Україна')]");
+        By code = By.xpath("//*[contains(text(), 'Україна')]");
         By number = By.xpath("//input[@data-qa-node='phone-number']");
         By amount = By.xpath("//input[@data-qa-node='amount']");
         By cardFrom = By.xpath("//input[@data-qa-node='numberdebitSource']");
         By debitSource = By.xpath("//input[@data-qa-node='expiredebitSource']");
         By cvv = By.xpath("//input[@data-qa-node='cvvdebitSource']");
+        By firstNamedebitSource = By.xpath("//input[@data-qa-node='firstNamedebitSource']");
+        By lastNamedebitSource = By.xpath("//input[@data-qa-node='lastNamedebitSource']");
         By button = By.xpath("//button[@data-qa-node='submit']");
-
+        By button2 = By.xpath("//*[contains(text(), 'Сплатити')]");
 
 
         driver.get("https://next.privat24.ua/mobile");
 
-        driver.findElement(number).sendKeys("7311111");
+        driver.findElement(number).sendKeys("939155859");
         driver.findElement(amount).sendKeys("\b\b\b");
-        driver.findElement(amount).sendKeys("111");
+        driver.findElement(amount).sendKeys("1");
         driver.findElement(cardFrom).sendKeys("4004159115449003");
         driver.findElement(debitSource).sendKeys("11/24");
         driver.findElement(cvv).sendKeys("198");
+        driver.findElement(firstNamedebitSource).sendKeys("CAT");
+        driver.findElement(lastNamedebitSource).sendKeys("Kitty");
         driver.findElement(button).click();
 
+
+        By actualAmount = By.xpath("//div[@data-qa-node='amount']");
+        By actualCommission = By.xpath("//span[@data-qa-node='commission']");
+        By actualDetails = By.xpath("//div[@data-qa-node='details']");
+
+
+        Assertions.assertEquals("1 UAH", driver.findElement(actualAmount).getText());
+        Assertions.assertEquals("2", driver.findElement(actualCommission).getText());
+        Assertions.assertEquals("Поповнення телефону. На номер +380939155859", driver.findElement(actualDetails).getText());
 
 
     }
